ad95daeff99e9e39f50e9d855746ce5f91e0bf39
NJ-7021 <nikhil1402m@gmail.com>
2025-03-19 17:09:50 +0530

    latest

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/tanikatechjewels/whatsapp-widget', // 👈 Add this line
+  base: 'https://github.com/tanikatechjewels/whatsapp-widget', 
+  server: {
+    proxy: {
+      // This proxy setup will forward requests to GitHub
+      '/whatsapp-widget': {
+        target: 'https://github.com', // Target the domain you want to proxy
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/whatsapp-widget/, ''), // Strip the /whatsapp-widget from the path
+      },
+    },
+  },
+
 });
